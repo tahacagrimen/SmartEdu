0c36e6b2c5881c06b4909ef83c39865d14527817
tahacagrimen <tahacagrimen2@gmail.com>
2022-07-28 14:38:00 +0300

    Smartedu 2

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const mongooose = require("mongoose");
+const pageRotue = require("./routes/pageRoute.js");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+await mongoose
+  .connect("mongodb://localhost/smartedu-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Template Engine i yüklüyoruz
+app.set("view engine", "ejs");
+
+// Static dosyaları middlewares ile bağlamak
+app.use(express.static("public"));
+
+// Routes
+app.use("/", pageRotue);
 
 const port = 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
